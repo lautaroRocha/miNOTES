d36742200e5f40d3c9536a7792f861122f3597af
Lautaro Rocha <lautarovrocha@gmail.com>
2022-08-23 15:11:03 -0300

    firebase deploy setup

--- src/components/NewNote.jsx
+++ src/components/NewNote.jsx
@@ -36,10 +36,9 @@ function NewNote(props){
            </textarea>
            <div className="new-note-btns">
             <button className="cancel" onClick={cancel}>
-            🗑️
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z"/></svg>
             </button>
             <input type="color" id="colorPicker" onChange={colourCard} list="presetColors" defaultValue='#455a64'/>
-           
                 <datalist id="presetColors">
                     <option>#455a64</option>
                     <option>#C8566B</option>
@@ -50,7 +49,10 @@ function NewNote(props){
                     <option>#6661AB</option>
                 </datalist>
             <button className="add" onClick={add}>
-            ✔️
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M9 22l-10-10.598 2.798-2.859 7.149 7.473 13.144-14.016 2.909 2.806z"/></svg>
+                
+
+
             </button>
            </div>
         </div>
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -87,14 +87,14 @@ function Note(props){
                <div className="new-note-btns">
                {origin !== "/fav" &&
                 <button className="cancel" onClick={dispose}>
-                🗑️
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z"/></svg>
                 </button>}
                 {origin !== "/fav" &&
                 <button className="cancel" onClick={edit}>
-                ✏️
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7.127 22.562l-7.127 1.438 1.438-7.128 5.689 5.69zm1.414-1.414l11.228-11.225-5.69-5.692-11.227 11.227 5.689 5.69zm9.768-21.148l-2.816 2.817 5.691 5.691 2.816-2.819-5.691-5.689z"/></svg>
                 </button>}
                 <button className="add" onClick={save}>
-                ✔️
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M9 22l-10-10.598 2.798-2.859 7.149 7.473 13.144-14.016 2.909 2.806z"/></svg>
                 </button>
                </div>
             </div>
