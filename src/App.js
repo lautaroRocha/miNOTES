ab004a6ac189690fae8c3b3df20510916cd0a825
Lautaro Rocha <lautarovrocha@gmail.com>
2022-08-30 18:19:05 -0300

    improved ux on favs functionality

--- src/App.js
+++ src/App.js
@@ -64,29 +64,12 @@ function App() {
         localStorage.setItem('favs', JSON.stringify(tempFavs));
         setFavs(tempFavs)
     }else{
-      MySwal.fire({
-        customClass: {
-            confirmButton: "confirm-btn",
-            popup : "swal-cont"
-        },
-        title : '¿🗑️?',
-        showCancelButton: true,
-        confirmButtonText : <svg xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" fill="#fff" viewBox="0 0 24 24"><path d="M9 22l-10-10.598 2.798-2.859 7.149 7.473 13.144-14.016 2.909 2.806z"/></svg> ,
-        cancelButtonText :<svg xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" fill="#fff"viewBox="0 0 24 24"><path d="M23 20.168l-8.185-8.187 8.185-8.174-2.832-2.807-8.182 8.179-8.176-8.179-2.81 2.81 8.186 8.196-8.186 8.184 2.81 2.81 8.203-8.192 8.18 8.192z"/></svg> ,
-        denyButtonText: `Don't save`
-    })
-    .then( (result) => {
-      if(result.isConfirmed){
-        let notesLeft = tempFavs.filter(note => {
-          return note.title !== favNote.title;})
-          localStorage.setItem('favs', JSON.stringify(notesLeft))
-          setFavs(notesLeft)
+        let notesLeft = tempFavs.filter(note => { return note.title !== favNote.title;})
+        localStorage.setItem('favs', JSON.stringify(notesLeft))
+        setFavs(notesLeft)
       }
-    })
-      
     }
-  
-    }
+
 
   return (
   <BrowserRouter>
