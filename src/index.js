03706060b601be2ff042c47e1c7499a526f5ea5b
Lautaro Rocha <102425176+lautaroRocha@users.noreply.github.com>
2022-09-12 16:30:51 -0300

    Revert "Firebase"

--- src/index.js
+++ src/index.js
@@ -2,24 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { initializeApp } from 'firebase/app'
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDW_hg8Zbngw2tvRH-rWjgdJXunrQFqmek",
-  authDomain: "minote-cf0e1.firebaseapp.com",
-  projectId: "minote-cf0e1",
-  storageBucket: "minote-cf0e1.appspot.com",
-  messagingSenderId: "454154376558",
-  appId: "1:454154376558:web:bbff2a2ffe39c258139d7f",
-  measurementId: "G-DK9097HGCB"
-};
-
-const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App app={app}/>
+    <App />
   </React.StrictMode>
 );
 
